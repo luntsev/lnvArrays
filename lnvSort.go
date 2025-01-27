788d43f77a3a0cb0f778e57955e6407b8fcff249
Nikolay Luntsev <n.luntsev@yandex.ru>
2025-01-27 20:38:24 +0300

    Добавил сортировку пузырьковую

---
+++ lnvSort.go
@@ -0,0 +1,30 @@
+package lnvArrays
+
+type ArrayItem struct {
+	priority int
+	object   interface{}
+}
+
+type Array []ArrayItem
+
+func bubbleAscSort(slice *Array) *Array {
+	for i := range *slice {
+		for j := 0; j < len(*slice)-i-1; j++ {
+			if (*slice)[j].priority > (*slice)[j+1].priority {
+				(*slice)[j], (*slice)[j+1] = (*slice)[j+1], (*slice)[j]
+			}
+		}
+	}
+	return slice
+}
+
+func bubbleDescSort(slice *Array) *Array {
+	for i := range *slice {
+		for j := 0; j < len(*slice)-i-1; j++ {
+			if (*slice)[j].priority > (*slice)[j+1].priority {
+				(*slice)[j], (*slice)[j+1] = (*slice)[j+1], (*slice)[j]
+			}
+		}
+	}
+	return slice
+}
