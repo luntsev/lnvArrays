986d097e36f00aee1a67d6c664c805a5809ae91a
Nikolay Luntsev <n.luntsev@yandex.ru>
2025-01-27 19:51:28 +0300

    Использовал заглавные буквы в начале наименований переменных

--- lnvArrays.go
+++ lnvArrays.go
@@ -4,27 +4,27 @@ import (
 	"errors"
 )
 
-type heapItem struct {
-	priority int
+type HeapItem struct {
+	Priority int
 	object   interface{}
 }
 
-type maxHeap []heapItem
-type minHeap []heapItem
+type MaxHeap []HeapItem
+type MinHeap []HeapItem
 
-func (pq *maxHeap) init() {
-	var nullItem heapItem
+func (pq *MaxHeap) init() {
+	var nullItem HeapItem
 	*pq = append((*pq)[:0], nullItem)
 }
 
-func (pq *maxHeap) push(elem heapItem) {
+func (pq *MaxHeap) push(elem HeapItem) {
 	*pq = append(*pq, elem)
 	if pq.len() > 1 {
 		pq.embed(pq.len() - 1)
 	}
 }
 
-func (pq *maxHeap) pop() (heapItem, error) {
+func (pq *MaxHeap) pop() (HeapItem, error) {
 	if pq.len() > 0 {
 		rootElem := (*pq)[0]
 		pq.swap(0, pq.len()-1)
@@ -34,34 +34,34 @@ func (pq *maxHeap) pop() (heapItem, error) {
 		}
 		return rootElem, nil
 	} else {
-		var nullElement heapItem
+		var nullElement HeapItem
 		err := errors.New("The heap is empty, there is nothing to return")
 		return nullElement, err
 	}
 }
 
-func (pq *maxHeap) get() heapItem {
+func (pq *MaxHeap) get() HeapItem {
 	rootElem := (*pq)[0]
 	return rootElem
 }
 
-func (pq *maxHeap) swap(i, j int) {
+func (pq *MaxHeap) swap(i, j int) {
 	(*pq)[i], (*pq)[j] = (*pq)[j], (*pq)[i]
 }
 
-func (pq *maxHeap) len() int {
+func (pq *MaxHeap) len() int {
 	return len(*pq)
 }
 
-func (pq *maxHeap) embed(itemId int) {
+func (pq *MaxHeap) embed(itemId int) {
 	if itemId > 0 {
 		if (itemId)%2 != 0 {
-			if (*pq)[itemId].priority > (*pq)[(itemId-1)/2].priority {
+			if (*pq)[itemId].Priority > (*pq)[(itemId-1)/2].Priority {
 				pq.swap(itemId, (itemId-1)/2)
 				pq.embed((itemId - 1) / 2)
 			}
 		} else {
-			if (*pq)[itemId].priority > (*pq)[(itemId-2)/2].priority {
+			if (*pq)[itemId].Priority > (*pq)[(itemId-2)/2].Priority {
 				pq.swap(itemId, (itemId-2)/2)
 				pq.embed((itemId - 2) / 2)
 			}
@@ -69,35 +69,35 @@ func (pq *maxHeap) embed(itemId int) {
 	}
 }
 
-func (pq *maxHeap) rebuild(itemId int) {
+func (pq *MaxHeap) rebuild(itemId int) {
 	switch {
 	case (itemId*2)+1 == pq.len()-1:
 		leftId := (itemId * 2) + 1
-		if (*pq)[leftId].priority > (*pq)[itemId].priority {
+		if (*pq)[leftId].Priority > (*pq)[itemId].Priority {
 			pq.swap(itemId, leftId)
 		}
 	case (itemId*2)+2 == pq.len()-1:
 		leftId := (itemId * 2) + 1
 		rightId := (itemId * 2) + 2
-		if (*pq)[leftId].priority > (*pq)[rightId].priority {
-			if (*pq)[leftId].priority > (*pq)[itemId].priority {
+		if (*pq)[leftId].Priority > (*pq)[rightId].Priority {
+			if (*pq)[leftId].Priority > (*pq)[itemId].Priority {
 				pq.swap(itemId, leftId)
 			}
 		} else {
-			if (*pq)[rightId].priority > (*pq)[itemId].priority {
+			if (*pq)[rightId].Priority > (*pq)[itemId].Priority {
 				pq.swap(itemId, rightId)
 			}
 		}
 	case (itemId*2)+2 < pq.len()-1:
 		leftId := (itemId * 2) + 1
 		rightId := (itemId * 2) + 2
-		if (*pq)[leftId].priority > (*pq)[rightId].priority {
-			if (*pq)[leftId].priority > (*pq)[itemId].priority {
+		if (*pq)[leftId].Priority > (*pq)[rightId].Priority {
+			if (*pq)[leftId].Priority > (*pq)[itemId].Priority {
 				pq.swap(itemId, leftId)
 				pq.rebuild(leftId)
 			}
 		} else {
-			if (*pq)[rightId].priority > (*pq)[itemId].priority {
+			if (*pq)[rightId].Priority > (*pq)[itemId].Priority {
 				pq.swap(itemId, rightId)
 				pq.rebuild(rightId)
 			}
