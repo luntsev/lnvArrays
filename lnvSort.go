da7b8eccb7de77851ef6c33897819a89faf7637f
Nikolay Luntsev <n.luntsev@yandex.ru>
2025-01-28 21:39:19 +0300

    Сортировка

--- lnvSort.go
+++ lnvSort.go
@@ -1,13 +1,16 @@
 package lnvArrays
 
+// Структура элемента массива, где priority - его приоритет, а object - любой объект
 type ArrayItem struct {
 	priority int
 	object   interface{}
 }
 
+// Массив элементов
 type Array []ArrayItem
 
-func BubbleAscSort(slice *Array) *Array {
+// Метод пузырьковой сортировки по возрастанию
+func (slice *Array) AscBubbleSort() {
 	for i := range *slice {
 		for j := 0; j < len(*slice)-i-1; j++ {
 			if (*slice)[j].priority > (*slice)[j+1].priority {
@@ -15,10 +18,10 @@ func BubbleAscSort(slice *Array) *Array {
 			}
 		}
 	}
-	return slice
 }
 
-func BubbleDescSort(slice *Array) *Array {
+// Метод пузырьковой сортировки по убыванию
+func (slice *Array) DescBubbleSort() {
 	for i := range *slice {
 		for j := 0; j < len(*slice)-i-1; j++ {
 			if (*slice)[j].priority > (*slice)[j+1].priority {
@@ -26,5 +29,18 @@ func BubbleDescSort(slice *Array) *Array {
 			}
 		}
 	}
-	return slice
+}
+
+// Метод сортировки слиянием по возрастанию
+func (slice *Array) AscMergeSort() {
+	if len(*slice) > 1 {
+		m := len(*slice) / 2
+		left := (*slice)[0:m]
+		right := (*slice)[m:len(*slice)]
+		slice = mergeArray(mergeSort(&left), mergeSort(&right))
+	}
+}
+
+func mergeAscArrays(left, right *Array) {
+
 }
